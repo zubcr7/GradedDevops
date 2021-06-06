14e2e7c26ac02b81d22de9237a55ea937bddfc70
zubcr7 <zubairahmed1729@gmail.com>
2021-06-06 23:03:43 +0530

    initial commit

---
+++ src/main/java/com/example/demo/HelloDevopsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDevopsApplication.class, args);
+		System.out.println("Hello Devops");
+	}
+
+}
